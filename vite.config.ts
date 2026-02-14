637583b41496095e013f4512b8fcd46c0739d7f5
Jonathan Liew <jliew@epic.com>
2026-02-14 12:21:41 -0600

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/valentine/',
+  base: '/valentine-day/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
